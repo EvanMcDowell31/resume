48980b6bd55192bfa21854c7fc66b9d4546dc4b0
EvanMcDowell31 <evan@evan.attlocal.net>
2018-12-12 21:02:13 -0500

    using router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
 import { ConnectedRouter } from "connected-react-router/immutable";
 import history from "utils/history";
 import "sanitize.css/sanitize.css";
 
 // Import root app
 import App from "containers/App";
-import configureStore from "./configureStore";
 
-// Create redux store with history
-const initialState = {};
-const store = configureStore(initialState, history);
 const MOUNT_NODE = document.getElementById("app");
 
 const render = () => {
   ReactDOM.render(
-    <Provider store={store}>
-      <ConnectedRouter history={history}>
-        <App />
-      </ConnectedRouter>
-    </Provider>,
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>,
     MOUNT_NODE
   );
 };
